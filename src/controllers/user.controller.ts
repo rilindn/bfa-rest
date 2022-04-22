e89bb7053e22c598560831259302a13a501030bf
rilind nuha <rn48707@ubt-uni.net>
2022-04-23 01:27:50 +0200

    feat: improve users crud

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -7,7 +7,7 @@ const getAllUsers = async (req: Request, res: Response) => {
     const result = await User.findAll()
     return res.send(result)
   } catch (error) {
-    return res.sendStatus(500).send(error)
+    return res.status(500).send(error)
   }
 }
 
@@ -16,10 +16,10 @@ const getUserById = async (req: Request, res: Response) => {
   try {
     if (!id) return
     const result = await User.findByPk(id)
-    if (!result) return res.sendStatus(404)
+    if (!result) return res.status(404)
     return res.send(result)
   } catch (error) {
-    return res.sendStatus(500).send(error)
+    return res.status(500).send(error)
   }
 }
 
@@ -28,14 +28,14 @@ const registerUser = async (req: Request, res: Response) => {
 
   if (validationResult.error) {
     const errorMsg = validationResult.error.details[0].message
-    return res.sendStatus(400).json({ error: errorMsg })
+    return res.status(400).send({ error: errorMsg })
   }
 
   try {
     const result = await User.create({ ...req.body })
     return res.send(result)
   } catch (error) {
-    return res.sendStatus(500).send(error)
+    return res.status(500).send(error)
   }
 }
 
@@ -45,16 +45,16 @@ const updateUser = async (req: Request, res: Response) => {
 
   if (validationResult.error) {
     const errorMsg = validationResult.error.details
-    return res.sendStatus(400).json({ error: errorMsg })
+    return res.status(400).json({ error: errorMsg })
   }
 
   try {
     const updatedUser = await User.update({ ...req.body }, { where: { id: userId } })
-    if (!updatedUser) return res.sendStatus(404).send('User not found!')
+    if (!updatedUser) return res.status(404).send('User not found!')
     const result = await User.findByPk(userId)
     return res.send(result)
   } catch (error) {
-    res.sendStatus(500).send(error)
+    res.status(500).send(error)
   }
 }
 
@@ -62,13 +62,13 @@ const deleteUser = async (req: Request, res: Response) => {
   try {
     const result = await User.findByPk(req.params.id)
     if (!result) {
-      return res.sendStatus(404)
+      return res.status(404)
     } else {
       await result.destroy()
       return res.send(result)
     }
   } catch (error) {
-    return res.sendStatus(500).send(error)
+    return res.status(500).send(error)
   }
 }
 
