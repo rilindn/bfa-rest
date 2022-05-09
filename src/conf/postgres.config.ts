aeda677d6a4f171c45678e507d74b4a0e2302fd7
rilind nuha <rn48707@ubt-uni.net>
2022-05-09 15:29:38 +0200

    fix: set postgres prod environment

--- src/conf/postgres.config.ts
+++ src/conf/postgres.config.ts
@@ -1,6 +1,13 @@
 import { Sequelize } from 'sequelize'
 require('dotenv').config()
-const { HOST, DB_NAME, DB_USERNAME, DB_PASSWORD } = process.env
+let { HOST, DB_NAME, DB_USERNAME, DB_PASSWORD } = process.env
+
+if (process.env.NODE_ENV === 'production') {
+  HOST = process.env.HOST_PROD
+  DB_NAME = process.env.DB_NAME_PROD
+  DB_USERNAME = process.env.DB_USERNAME_PROD
+  DB_PASSWORD = process.env.DB_PASSWORD_PROD
+}
 
 export const sequelize = new Sequelize(DB_NAME!, DB_USERNAME!, DB_PASSWORD, {
   host: HOST,
