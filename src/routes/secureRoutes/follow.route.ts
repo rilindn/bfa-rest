f73764c213cbdd79050edcafe4e4ff7b2f37fbfe
rilind nuha <rn48707@ubt-uni.net>
2022-05-16 23:07:23 +0200

    feat: get follow suggestions

--- src/routes/secureRoutes/follow.route.ts
+++ src/routes/secureRoutes/follow.route.ts
@@ -9,6 +9,8 @@ router.get('/followings/:id', FollowController.getMyFollowings)
 
 router.get('/checkFollow/:followerId/:followedId', FollowController.checkFollow)
 
+router.get('/suggestions/:id', FollowController.getFollowSuggestions)
+
 router.post('/', FollowController.createFollow)
 
 router.delete('/:followerId/:followedId', FollowController.unFollow)
