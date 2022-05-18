4394829ed4c7757bbfe981cfb17a62d7dc963ecf
rilind nuha <rn48707@ubt-uni.net>
2022-05-18 02:55:28 +0200

    fix(posts): allow null on media

--- src/validators/post.validation.ts
+++ src/validators/post.validation.ts
@@ -3,12 +3,12 @@ import Joi from 'joi'
 export const registerSchema = Joi.object({
   UserId: Joi.string().required().label('User ID'),
   content: Joi.string().min(2).required().label('Content'),
-  media: Joi.string().label('Media'),
+  media: Joi.string().allow(null).label('Media'),
 })
 
 export const updateSchema = Joi.object({
   postId: Joi.string().required().label('ID'),
   UserId: Joi.string().label('User ID'),
   content: Joi.string().min(2).label('Content'),
-  media: Joi.string().label('Media'),
+  media: Joi.string().allow(null).label('Media'),
 })
