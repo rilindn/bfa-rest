9dae713cb9a2f2799c989fee7528d1572b665946
rilind nuha <rn48707@ubt-uni.net>
2022-06-03 01:50:19 +0200

    feat(messages): get chat by id and delete a particular message

--- src/routes/secureRoutes/chat.route.ts
+++ src/routes/secureRoutes/chat.route.ts
@@ -5,10 +5,14 @@ const router = express.Router()
 
 router.get('/:id', ChatController.getMyChats)
 
+router.get('/byId/:id', ChatController.getChatById)
+
 router.post('/:id', ChatController.newMessage)
 
 router.post('/', ChatController.createChat)
 
 router.delete('/:id', ChatController.deleteChat)
+
+router.delete('/:id/:messageId', ChatController.deleteMessage)
 
 export default router
