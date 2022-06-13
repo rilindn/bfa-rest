f547eee9acd1b72aedfad81947b87a6a29c46cd4
rilind nuha <rn48707@ubt-uni.net>
2022-06-13 02:11:05 +0200

    fix: allow admin in user validation role

--- src/validators/user.validation.ts
+++ src/validators/user.validation.ts
@@ -4,7 +4,7 @@ export const registerSchema = Joi.object({
   email: Joi.string().email().required().label('Email'),
   password: Joi.string().min(7).max(30).required().label('Password'),
   birthDate: Joi.string().label('Birthdate'),
-  role: Joi.string().required().valid('Player', 'Club').label('Role'),
+  role: Joi.string().required().valid('Player', 'Club', 'Admin').label('Role'),
   status: Joi.string().label('Status').valid('Active', 'Inactive'),
   profilePic: Joi.string().min(2).label('Profile picture'),
 })
@@ -14,7 +14,7 @@ export const updateSchema = Joi.object({
   email: Joi.string().email().label('Email'),
   password: Joi.string().min(7).max(30).label('Password'),
   birthDate: Joi.string().label('Birthdate'),
-  role: Joi.string().valid('Player', 'Club').label('Role'),
+  role: Joi.string().valid('Player', 'Club', 'Admin').label('Role'),
   status: Joi.string().label('Status').valid('Active', 'Inactive'),
   profilePic: Joi.string().min(2).label('Profile picture'),
 })
