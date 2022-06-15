0a9603750ae0b01271bcbfc611613377614872e9
rilind nuha <rn48707@ubt-uni.net>
2022-06-15 23:13:28 +0200

    fix: add isSuperAdmin in admin validatior

--- src/validators/admin.validation.ts
+++ src/validators/admin.validation.ts
@@ -4,10 +4,12 @@ import { registerSchema, updateSchema } from './user.validation'
 export const adminRegisterSchema = registerSchema.keys({
   firstName: Joi.string().min(2).max(30).required().label('Firstname'),
   lastName: Joi.string().min(2).max(30).required().label('Lastname'),
+  isSuperAdmin: Joi.bool(),
 })
 
 export const adminUpdateSchema = updateSchema.keys({
   userId: Joi.string().required().label('ID'),
   firstName: Joi.string().min(2).max(30).label('Firstname'),
   lastName: Joi.string().min(2).max(30).label('Lastname'),
+  isSuperAdmin: Joi.bool(),
 })
